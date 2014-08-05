d9cf8774d77bca08e86bf494aae9caf724a69701
Jim Holpuch <udsaxman@gmail.com>
2014-08-05 16:38:09 -0500

    Footer include

--- TDSInEditAccess.php
+++ TDSInEditAccess.php
@@ -128,24 +128,7 @@ include('header.html');
 
                 </div>
                 <!-- InstanceEndEditable -->
-                <footer align="center">
-                    EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of
-                    CCP hf.
-                    <br/>
-                    All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable
-                    features of the intellectual property relating to these trademarks are likewise the intellectual
-                    property of CCP hf.
-                    <br/>
-                    EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved
-                    worldwide. All other trademarks are the property of their respective owners.
-                    <br/>
-                    CCP hf. has granted permission to tdsin.net to use EVE Online and all associated logos and designs
-                    for promotional and information purposes on its website but does not endorse, and is not in any way
-                    affiliated with, tdsin.net.
-                    <br/>
-                    CCP is in no way responsible for the content on or functioning of this website, nor can it be liable
-                    for any damage arising from the use of this website.
-                </footer>
+                <?php include('footer.html'); ?>
             </DIV>
 
         </td>
