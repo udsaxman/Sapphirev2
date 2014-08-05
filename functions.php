d306a053dd17085fc5f9b57cca6210ae8706aef4
Jim Holpuch <udsaxman@gmail.com>
2014-08-05 16:18:42 -0500

    Added function to check if Op is still active

--- functions.php
+++ functions.php
@@ -17,4 +17,25 @@ function CheckAccess($pagename)
         Return $RankResult['access_power'];
 
 }
+
+function CheckOpActive($opID)
+{
+    include 'connection.php';
+
+    $sql = "SELECT
+                LiveOp_Active
+            FROM
+                LiveOps
+            WHERE
+                LiveOp_ID = ".$opID;
+
+    $result = $mysqli->query($sql);
+    $isactive= mysqli_fetch_assoc($result);
+    if ($isactive['Liveop_Active'] == 1){
+        return true;
+    }else{
+        return false;
+    }
+
+}
 ?>
