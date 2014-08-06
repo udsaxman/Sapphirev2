46d9c8ea75d8e290ab217db8ef5d32c0d06442d3
Jim Holpuch <udsaxman@gmail.com>
2014-08-06 16:40:15 -0500

    fixed liveop active check to not be bad

--- functions.php
+++ functions.php
@@ -21,18 +21,15 @@ function CheckAccess($pagename)
 function CheckOpActive($opID)
 {
     global $mysqli;
-
     $sql = "SELECT
                 LiveOp_Active
             FROM
                 LiveOps
             WHERE
                 LiveOp_ID = ".$opID;
-
     $result = $mysqli->query($sql);
-    $isactive= mysqli_fetch_assoc($result);
-
-    if ($isactive['Liveop_Active'] == 1){
+    $isactive= $result -> fetch_row();
+    if ($isactive[0] == 1){
         return true;
     }else{
         return false;
@@ -43,6 +40,7 @@ function CheckOpActive($opID)
 function LeaveOp($opID, $memberID)
 {
     global $mysqli;
+
 
     $sql = "CALL sp_LeaveLiveOp(" . $opID. ", " . $memberID . ")";
 
@@ -56,16 +54,12 @@ function JoinOp($opID, $memberID)
     global $mysqli;
 
     $sql = "CALL sp_JoinLiveOp(" . $opID. ", " . $memberID . ")";
-    //echo $sql;
+   // echo $sql;
     if($mysqli->query($sql) === TRUE){
-        $_SESSION["current_op"] = memebrID;
-        //echo $_SESSION["current_op"]."set";
+        $_SESSION["current_op"] = $opID;
+        echo $_SESSION["current_op"]."set";
     }
     $sql = "CALL sp_LeaveLiveOp(" . $opID. ", " . $memberID . ")";
 
-    if($mysqli->query($sql) === TRUE){
-        //player has left op, unset session variable
-        unset($_SESSION["current_op"]);
-    }
 }
 ?>
