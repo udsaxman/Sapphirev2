75f88b9f812545e495a011d4826b1aed37e40c31
Jim Holpuch <udsaxman@gmail.com>
2014-09-05 18:29:19 -0500

    Cleanup

--- processItemCategories.php
+++ processItemCategories.php
@@ -1,104 +1,48 @@
 <?php
 include 'connection.php';
 
-$oldCatIds[0] = 0;
-$oldCatNames[0] = "";
-$oldCatOrders[0] = 0;
-$oldCatTaxs[0] = 0;
-$oldCatOverride[0] = 0;
-$oldCatCount = 0;
+//Note to self, figure out userinput validation so people don't submit sql strings for update values
+foreach ($_POST['catupdate'] as $cat){
+    if(isset($cat['id']) && isset($cat['order'])&& isset($cat['tax'])){
 
-$validOldCount = 0;
-
-$newCatNames[0] = "";
-$newCatOrders[0] = 0;
-$newCatTaxs[0] = 0;
-$newCatOverride[0] = 0;
-
-$newCatCount = 0;
-
-$sql = "Select Count(category_id) As Total From Item_Category";
-
-$result = mysql_query($sql, $conn) or die(mysql_error());
-
-while ($row = mysql_fetch_assoc($result)) {
-    foreach ($row as $name => $value) {
-        if ($name == "Total") {
-            $oldCatCount = $value;
+        $sql = "CALL sp_UpdateItemCategory(".$cat['id'].",". $cat['order'].",". $cat['tax'].",".(isset($cat['override'])? 1 : 0)." )";
+        $result = $mysqli->query($sql);
+        if (!$result) {
+            Fail();
         }
+        $result->close();
     }
 }
 
-for ($i = 1; $i < $oldCatCount + 1; $i++) {
-    if (isset($_POST["oldCatName" . $i]) && $_POST["oldCatName" . $i] != "") {
-        $oldCatNames[$validOldCount] = $_POST["oldCatName" . $i];
-        $oldCatIds[$validOldCount] = $i;
+foreach ($_POST['newCat'] as $newcat){
+    print_r($newcat);
+    echo "</br>";
 
-        if (isset($_POST["oldCatOrder" . $i]) && is_numeric($_POST["oldCatOrder" . $i])) {
-            $oldCatOrders[$validOldCount] = $_POST["oldCatOrder" . $i];
-        } else {
-            $oldCatOrders[$validOldCount] = 0;
+    if(isset($newcat['name']) && isset($newcat['tax'])&& isset($newcat['order'])){
+
+        //create new category
+        $sql = "CALL sp_AddNewItemCategory('".$newcat['name']."', ".$newcat['order'].", ".$newcat['tax'].", ".(isset($newcat['override'])? 1 : 0).")";
+        $result = $mysqli->query($sql);
+        if (!$result) {
+            Fail();
         }
-        if (isset($_POST["oldCatTax" . $i]) && is_numeric($_POST["oldCatTax" . $i])) {
-            $oldCatTaxs[$validOldCount] = $_POST["oldCatTax" . $i];
-        } else {
-            $oldCatTaxs[$validOldCount] = 0;
-        }
-        if (isset($_POST["oldCatOverride" . $i])) {
-            $oldCatOverride[$validOldCount] = 1;
-        } else {
-            $oldCatOverride[$validOldCount] = 0;
-        }
-
-        $validOldCount++;
+        $result->close();
     }
 }
 
-while (isset($_POST["newCatName" . $newCatCount]) && $_POST["newCatName" . $newCatCount] != "") {
-    $newCatNames[$newCatCount] = $_POST["newCatName" . $newCatCount];
-
-    if (isset($_POST["newCatOrder" . $newCatCount]) && is_numeric($_POST["newCatOrder" . $newCatCount])) {
-        $newCatOrders[$newCatCount] = $_POST["newCatOrder" . $newCatCount];
-    } else {
-        $newCatOrders[$newCatCount] = 0;
-    }
-    if (isset($_POST["newCatTax" . $newCatCount]) && is_numeric($_POST["newCatTax" . $newCatCount])) {
-        $newCatTaxs[$newCatCount] = $_POST["newCatTax" . $newCatCount];
-    } else {
-        $newCatTaxs[$newCatCount] = 0;
-    }
-    if (isset($_POST["newCatOverride" . $newCatCount])) {
-        $newCatOverride[$newCatCount] = 1;
-    } else {
-        $newCatOverride[$newCatCount] = 0;
-    }
-
-    $newCatCount++;
-}
-
-for ($i = 0; $i < $validOldCount; $i++) {
-    $sql = "Call sp_UpdateItemCategory(" . $oldCatIds[$i]
-        . ", '" . $oldCatNames[$i] . "', " . $oldCatOrders[$i]
-        . ", " . $oldCatTaxs[$i] . ", " . $oldCatOverride[$i] . ")";
-    $result = mysql_query($sql, $conn) or die(mysql_error());
-}
-
-for ($i = 0; $i < $newCatCount; $i++) {
-    $sql = "Call sp_AddNewItemCategory('" . $newCatNames[$i]
-        . "', " . $newCatOrders[$i] . ", " . $newCatTaxs[$i] . ", " . $newCatOverride[$i] . ")";
-    $result = mysql_query($sql, $conn) or die(mysql_error());
-}
+$mysqli->close();
 
 Success();
 
-function Fail($error)
+function Fail()
 {
-    header('Location: ./TDSInError.php?Error=' . $error . '');
+   header('Location: ./TDSInError.php');
 }
 
 function Success()
 {
-    header('Location: ./TDSInAdminTools.php');
+    header('Location: ./TDSInItemCategories.php');
+    //echo "Test";
 }
 
 ?>
