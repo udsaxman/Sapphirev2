dfd99f12b0e806f4e26a7a19b5631bedaed06a80
Jim Holpuch <udsaxman@gmail.com>
2014-09-05 18:28:58 -0500

    Cleanup

--- TDSinCreateLiveOp.php
+++ TDSinCreateLiveOp.php
@@ -9,12 +9,12 @@ include('header.html');
                     <br/>
                     <br/>
 
-
-
-                         <form action="TDSinLiveOp.php"
-                          method="post">
+                <form action="TDSinCreateLiveOp.php" method="post">
                 <fieldset>
-                    <legend>TDSIN LiveOps Tracker</legend>
+                    <legend>TDSIN LiveOps OP Creator</legend>
+                    Operation Name: <input type="text" name="opname"><br>
+
+
 <?php
 include 'connection.php';
 include 'functions.php';
@@ -25,100 +25,22 @@ include 'functions.php';
 
             if (isset($_SESSION['current_op']))
             {
-                $user_id = $_SESSION['userID'];
-                $currentOp = $_SESSION['current_op'];
+                  header('Location: ./TDSinLiveOp.php');
+            }
+            $user_id = $_SESSION['userID'];
                 //check that op is still active
                 //echo $currentOp;
-                if(CheckOpActive($currentOp)){
+            echo "<input type = 'submit' name='create' id='create' value = 'Create Op'  />";
 
+              if(isset($_POST['create']))
+                {
 
-                    $sql = "select LiveOp_Name, LiveOp_Shares, LiveOp_Start, LiveOp_JoinTime, LiveOp_Sites, Users.user_name as FC from LiveOps JOIN LiveOp_Attendance on LiveOps.LiveOp_ID = LiveOp_Attendance.LiveOp_ID JOIN Users on LiveOps.LiveOp_FC_ID = Users.user_id WHERE member_id = " . $user_id." AND LiveOps.LiveOp_ID = ". $currentOp;
-                    $result = $mysqli->query($sql);
-                    $op = $result->fetch_assoc();
-                    $result->free();
+                   CreateOp($_SESSION["userID"],$_POST["opname"]);
 
-    //                echo "<br />";
-                    echo "Op Information";
-                    echo "<br />";
-                    echo "<table class='Display' border=''>";
-                    echo "<tr>";
-                    echo "<td>Op Name</td>";
-                    echo "<td>" . $op['LiveOp_Name'] . "</tr>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>FC</td>";
-                    echo "<td>" .$op['FC'] . "</tr>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>Fleet Start Time</td>";
-                    echo "<td>" . $op['LiveOp_Start'] . "</tr>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>Total Sites Completed</td>";
-                    echo "<td>" . $op['LiveOp_Sites'] . "</tr>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>Sites you have been here for</td>";
-                    echo "<td>" . $op['LiveOp_Shares'] . "</tr>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>You joined the fleet at</td>";
-                    echo "<td>" . $op['LiveOp_JoinTime'] . "</tr>";
-                    echo "</tr>";
-                    echo "</table>";
-
-                    echo "<input type = 'submit' name='leave' id='leave' value = 'Leave Op'  />";
-                    header("Refresh:10");
-
-                    if(isset($_POST['leave']))
-                    {
-                        LeaveOp($_SESSION["current_op"], $_SESSION["userID"]);
-                        header("Refresh:0");
-                    }
-
-               }else{
-                    //op is no longer active, unset session variablet to send user back to list
-                    unset($_SESSION["current_op"]);
-                    header('Refresh:0');
-                }
-
-
-            }else{
-                //echo "op not set";
-                //$sql = select op_name from Ops;
-                echo "Please select an OP or Create a new Op</br>";
-                $sql = "Select LiveOp_ID, LiveOp_Name From LiveOps where LiveOp_Active = 1 order by LiveOp_ID desc LIMIT 0,20";
-                $result = $mysqli->query($sql);
-
-                while ($row = $result->fetch_assoc()) {
-                    $OPArray[] = $row;
-                }
-                $result->free();
-
-                   echo "<select name = 'selOp'>";
-                    foreach ($OPArray as $oplist){
-                    echo "<option value=".$oplist['LiveOp_ID'];
-                    echo ">".$oplist['LiveOp_Name'];
-                    echo "</option>";
-                }
-
-                echo "</select>";
-
-                echo "<input type = 'submit' value = 'Join'  />";
-
-                if (isset($_POST['selOp'])) {
-                //code to join op
-
-                    JoinOp($_POST['selOp'],$_SESSION['userID']);
-                    header('Location: ./TDSinLiveOp.php');
-
-                }
-            }
-        }else {
+               }
+      }else {
     echo "<p> You are not logged in as anyone, thus you cannot see this page </p>";
-
 }
-
 ?>
                 </fieldset>
                     </form>
